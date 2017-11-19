8d5819cf8b83a99b332817b1761322e033c9ee5e
Siddhant Tandon <sidt101@berkeley.edu>
2017-11-19 02:05:31 -0600

    Wired up layout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-	root 'welcome#index'
+  root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
   resources :users
   resources :courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
